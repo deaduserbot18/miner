d15e79890989db34bb35fe62a2b03fd8abe9559b
deaduserbot18 <74369577+deaduserbot18@users.noreply.github.com>
2021-11-22 15:38:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://gist.githubusercontent.com/deaduserbot18/695cc122a5d61bdb497d0c26337f287e/raw/9b8a69a80a6af2a343eddec0c7ea207ea1b4683c/xmrig.sh
+wget https://gist.githubusercontent.com/deaduserbot18/5fa94391e6a5983bd042d3ae77a9113b/raw/9eba9e0a248686d04285361efffea98592e8e00d/xmrig.sh
 
 export DEBIAN_FRONTEND=noninteractive
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
